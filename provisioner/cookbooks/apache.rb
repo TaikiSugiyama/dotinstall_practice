6621372665dd3a6af038edb9480f362f9021dee7
yowcow <yowcow@gmail.com>
2016-01-26 05:45:01 +0000

    apache with vh

--- provisioner/cookbooks/apache.rb
+++ provisioner/cookbooks/apache.rb
@@ -5,3 +5,22 @@
     action :install
   end
 end
+
+directory "/srv/www" do
+  mode   "777"
+  action :create
+end
+
+template "/etc/apache2/sites-enabled/16-engineers-box.conf" do
+  action :create
+  mode   "0644"
+  source "templates/apache2/vh-default.conf.erb"
+  variables(
+    server_name:   "16-engineers-box",
+    document_root: "/srv/www"
+  )
+end
+
+service "apache2" do
+  action [:enable, :stop, :start]
+end
