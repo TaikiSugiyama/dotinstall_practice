3fb6569c83313323713829731de76b178c0397ca
TaikiSugiyama <homenobiz@gmail.com>
2016-02-28 15:48:45 +0000

    php_db_10

--- php_db_lessons/index.php
+++ php_db_lessons/index.php
@@ -10,8 +10,16 @@ try{
 	$db = new PDO(PDO_DSN, DB_USERNAME, DB_PASSWORD);
 	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	//select all
-        $stmt = $db->query("select * from users");
-        $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        //$stmt = $db->query("select * from users");
+        //$stmt = $db->prepare("select score from users where score > ?");
+        //$stmt->execute([20]);
+        //$stmt = $db->prepare("select name from users where name like ?");
+        //$stmt->execute(['%y%']);
+        $stmt = $db->prepare("select score from users order by score desc limit ?");
+        $stmt->bindValue(1,1,PDO::PARAM_INT);
+        $stmt->execute();
+        //$stmt->execute([1]);
+	$users = $stmt->fetchAll(PDO::FETCH_ASSOC);
         foreach($users as $user){
                 var_dump($user);
         }
