a3f376baa0b83e400c1282f51879f15da909afe6
TaikiSugiyama <homenobiz@gmail.com>
2016-02-28 16:52:20 +0000

    php_db_12

--- php_db_lessons/index.php
+++ php_db_lessons/index.php
@@ -5,27 +5,25 @@ define('DB_USERNAME','dbuser');
 define('DB_PASSWORD','sakura39');
 define('PDO_DSN','mysql:dbhost=localhost;dbname='.DB_DATABASE);
 
-class User{
-        //public $id;
-        //public $name;
-        //public $score;
-        public function show(){
-                echo "$this->name($this->score)";
-        }
-}
-
 try{
 	//connect
 	$db = new PDO(PDO_DSN, DB_USERNAME, DB_PASSWORD);
 	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	//select all
-	//FETCH_CLASS
-        $stmt = $db->query("select * from users");
-        $users = $stmt->fetchAll(PDO::FETCH_CLASS, 'User');
-        foreach($users as $user){
-                $user->show();
-        }
-        echo $stmt->rowCount()."records found.";
+	//update
+        $stmt = $db->prepare("update users set score = :score where name = :name");
+        $stmt->excute([
+                'score'=>100,
+                ':name'=>'taguchi'
+        ]);
+        echo 'row updated: '.$stmt->rowCount();
+
+
+        //delete
+        $stmt = $db->prepare("delete fron users where name = :name");
+        $stmt->excute([
+                ':name'=>'totinstall'
+        ]);
+        echo 'row deleted: '.$stmt->rowCount();
 }catch(PDOException $e){
 	echo $e->getMessage();
 	exit;
