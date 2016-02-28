b1bb74b1bb9e12c547683d803d9594c7946ebd2c
TaikiSugiyama <homenobiz@gmail.com>
2016-02-28 13:10:13 +0000

    php_db_5

--- php_db_lessons/index.php
+++ php_db_lessons/index.php
@@ -10,13 +10,14 @@ try{
 	$db = new PDO(PDO_DSN, DB_USERNAME, DB_PASSWORD);
 	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-	//insert
-	$db->exec("insert into users (name, score) values('sugiyama', 55)");
-	echo "user added!";
-
-	//disconnect
-	$db = null;
-	
+	/*
+	(1)exec():結果を返さない,安全なSQL
+	(2)query():結果を返す、安全、何回も実行されないSQL
+	(3)prepare():結果を返す、安全対策が必要、複数回実行されるSQL
+	*/
+	$stmt->db->prepare("insert into users (name, score) values(?,?)");
+	$stmt->execute(['sugiyama',44]);
+	echo "inserted: ".$db->lastInsertId();	
 
 }catch(PDOException $e){
 	echo $e->getMessage();
