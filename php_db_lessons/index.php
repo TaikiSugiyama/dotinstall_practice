00ffefa0f19efa4ef709af70e34182faf2cd1467
TaikiSugiyama <homenobiz@gmail.com>
2016-02-28 14:49:20 +0000

    php_db_7

--- php_db_lessons/index.php
+++ php_db_lessons/index.php
@@ -10,12 +10,18 @@ try{
 	$db = new PDO(PDO_DSN, DB_USERNAME, DB_PASSWORD);
 	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-	
-	//$stmt->db->prepare("insert into users (name, score) values(?,?)");
-	//$stmt->execute(['sugiyama',44]);
-	$stmt->db->prepare("insert into users (name, score) values(:name,:score)");
-        $stmt->execute([':name' =>'sato',':score'=>80]);
-	echo "inserted: ".$db->lastInsertId();	
+	//insert
+	//bindValue
+	$stmt = $db->prepare("insert into users (name, score) values(?,?)");
+        $name = 'sugiyama';
+	$stmt->bindValue(1, $name,PDO::PARAM_STR);
+	$score = 23;
+        $stmt->bindValue(2, $score,PDO::PARAM_INT);
+        $stmt->excute();
+
+	$score = 44;
+        $stmt->bindValue(2,$score,PDO::PARAM_INT);
+        $stmt->execute();
 
 }catch(PDOException $e){
 	echo $e->getMessage();
